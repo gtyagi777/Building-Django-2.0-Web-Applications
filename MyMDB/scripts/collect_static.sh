480e37fc62cecac9233d3ea0e28e4d5746de66ca
Abay Bek <bekbaganbetov.abay@gmail.com>
2018-12-21 14:16:28 +0600

    Chapter 5: end

--- MyMDB/scripts/collect_static.sh
+++ MyMDB/scripts/collect_static.sh
@@ -6,7 +6,7 @@ source $root/venv/bin/activate
 export DJANGO_CACHE_TIMEOUT=100
 export DJANGO_SECRET_KEY=FAKE_KEY
 export DJANGO_SETTINGS_MODULE=config.settings.production_settings
-
+export DJANGO_LOG_FILE=/var/log/mymdb/mymdb.log
 cd $root/django/
 
 python manage.py collectstatic
